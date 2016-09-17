9df28faf44b8ad1ffe0bd47fceace07ab86aa943
Alex Feather <alexfeather77@gmail.com>
2016-09-17 05:41:52 +0000

    Updated Resume Files

--- doCommit.sh
+++ doCommit.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+set -e
 
 #make sure the PDF has been created
-pdflatex Resume.tex
+pdflatex Resume.tex > /dev/null
+echo "Resume.pdf created..."
 
 #create our PNG image
 pdftoppm -f 1 -singlefile -png Resume.pdf Resume
+echo "Resume.png created..."
 
 #create our TXT file
 pdftotext Resume.pdf Resume.txt
+echo "Resume.txt created..."
 
 #add the new files to the repo
 git add Resume.tex Resume.pdf Resume.png Resume.txt README.md doCommit.sh

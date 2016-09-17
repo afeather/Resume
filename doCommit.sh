ce2e5170ddc99018d8f38138a4d9cc68c7d78744
Alex Feather <alexfeather77@gmail.com>
2016-09-17 05:37:19 +0000

    Updated Resume Files

--- doCommit.sh
+++ doCommit.sh
@@ -6,8 +6,11 @@ pdflatex Resume.tex
 #create our PNG image
 pdftoppm -f 1 -singlefile -png Resume.pdf Resume
 
+#create our TXT file
+pdftotext Resume.pdf Resume.txt
+
 #add the new files to the repo
-git add Resume.tex Resume.pdf Resume.png README.md doCommit.sh
+git add Resume.tex Resume.pdf Resume.png Resume.txt README.md doCommit.sh
 git commit -m "Updated Resume Files"
 
 #push local copy to github

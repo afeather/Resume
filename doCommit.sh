5c66ff46840582eb9eb0613117b842766a2c940c
Alex Feather <alexfeather77@gmail.com>
2023-01-01 00:45:59 -0500

    Updated Resume Files

--- doCommit.sh
+++ doCommit.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 set -e
 
-#make sure the PDF has been created
-pdflatex -interaction=nonstopmode Resume.tex > null
-echo "Resume.pdf created..."
+# Make sure the PDF has been created.
+pdflatex -interaction=nonstopmode Resume.tex && echo "Resume.pdf created..."
 
-#create our PNG image
-pdftoppm -f 1 -singlefile -png Resume.pdf Resume
-echo "Resume.png created..."
+# Create our PNG image
+pdftoppm -f 1 -singlefile -png Resume.pdf Resume && echo "Resume.png created..."
 
-#create our TXT file
-pdftotext Resume.pdf Resume.txt
-echo "Resume.txt created..."
+# Create our TXT file
+pdftotext Resume.pdf Resume.txt & echo "Resume.txt created..."
 
-#add the new files to the repo
+# Add the new files to the repo
 git add Resume.tex Resume.pdf Resume.png Resume.txt README.md doCommit.sh
 git commit -m "Updated Resume Files"
 
-#push local copy to github
+# Push
 git push -u origin master
